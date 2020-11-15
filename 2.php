ebe4e6358f0d41b11d1ba1594b9552c5e3094d5d
Vladislav Artemov <42673508+Zblol@users.noreply.github.com>
2020-11-15 13:44:45 +0300

    added importXml(); added 2.sql added 2.xml added.dataB_conn

--- 2.php
+++ 2.php
@@ -66,3 +66,94 @@ function mySortForKey($a, $b)
 ];
 
 print_r(mySortForKey($a, 'b'));*/
+
+
+/**Реализовать функцию importXml($a). $a – путь к xml файлу (структура файла приведена ниже).
+ * Результат ее выполнения: прочитать файл $a и импортировать его в созданную БД.
+ */
+
+
+function importXml($a)
+{
+    require_once("dataB_conn.php");
+    $xmlDoc = file_get_contents($a);
+    $data = new SimpleXmlElement($xmlDoc);
+
+    try {
+
+        for ($i = 0, $z = 0, $parentId = 0; $i < $data->{'Товар'}->count(); $i++) {
+
+            $productName = $data->{'Товар'}[$i]->attributes()->{'Название'};
+            $kod = $data->{'Товар'}[$i]->attributes()->{'Код'};
+            $price = $data->{'Товар'}[$i]->{'Цена'};
+            $property = $data->{'Товар'}[$i]->{'Свойства'};
+
+
+            $findData = $pdo->query("SELECT * FROM  a_product WHERE название LIKE '$productName'");
+
+            if ($findData->fetch()) {
+                $addProduct = $pdo->prepare("INSERT INTO a_product VALUES (NULL,?,?)");
+                if ($data->{'Товар'}[$i]->attributes()->{'Код'}) {
+
+                    $addProduct->execute([$kod, $productName]);
+
+                } else {
+                    $addProduct->execute([NULL, $productName]);
+                }
+
+                $productId = $pdo->lastInsertId();
+
+                $addPrice = $pdo->prepare("INSERT INTO a_price VALUES ($productId,?,?)");
+                foreach ($price as $val) {
+                    $addPrice->execute([$val['Тип'], $val]);
+                }
+
+                $addProperties = $pdo->prepare("INSERT INTO a_property VALUES ($productId,?,?)");
+                foreach ($property as $propsName) {
+
+                    foreach ($propsName as $prop => $val) {
+
+                        if ($property) {
+                            $addProperties->execute([$prop], [$val]);
+                        }
+
+                    }
+
+                }
+
+                $addCategory = $pdo->prepare("INSERT INTO a_category VALUES (NULL,?,?,?)");
+                $productInCategory = $pdo->prepare("INSERT INTO a_product_category VALUES (?,?)");
+                foreach ($data->{'Товар'}[$i]->{'Разделы'}->{'Раздел'} as $categoryName) {
+
+                    $queryCategory = $pdo->query("SELECT * FROM a_category WHERE название LIKE '$categoryName'");
+                    $category = $queryCategory->fetch(PDO::FETCH_ASSOC);
+                    if ($category["Название"] == $categoryName) {
+
+                        $parentId = $category["категория_ид"];
+                        $productInCategory->execute([$productId, $category["категория_ид"]]);
+
+                    } else {
+
+                        $addCategory->execute([$z, $categoryName, $parentId]);
+                        $parentId = $pdo->lastInsertId();
+                        $productInCategory->execute([$productId, $pdo->lastInsertId()]);
+                        $z++;
+
+
+                    }
+
+                }
+
+                $parentId = 0;
+            }
+        }
+    } catch (PDOException $e) {
+
+        echo "Ошибка загрузки " . $e->getMessage();
+    }
+
+
+}
+
+//$a = "2.xml";
+//importXml($a);
